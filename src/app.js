4380f62ffdfca3c078fff7acbed2b82249884cdd
Catalina Munevar <catalinamunevar@gmail.com>
2023-06-21 12:02:42 -0400

    Updated CCS and added background

--- src/app.js
+++ src/app.js
@@ -67,7 +67,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "eae061c95483dd066657bfc7525418ed";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
